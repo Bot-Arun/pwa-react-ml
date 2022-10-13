1f9899326538a32f5edff7e1a9fc1bff8097c58a
Arun <arunmugunthan07@gmail.com>
2022-10-13 19:17:55 +0530

    added fcm

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,6 +1,10 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js'
-import {getMessaging , getToken,onMessage} from 'https://www.gstatic.com/firebasejs/9.10.0/firebase-messaging.jsfirebase/messaging'
-const firebaseConfig = {
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js');
+importScripts("https://cdn.jsdelivr.net/npm/idb-keyval@6/dist/umd.js");
+// Initialize the Firebase app in the service worker by passing in
+// your app's Firebase config object.
+// https://firebase.google.com/docs/web/setup#config-object
+const firebaseApp = firebase.initializeApp( {
     apiKey: "AIzaSyBTmLFT2eXjSz4U01Kxo8uQ4yL5Af0qUSg",
     authDomain: "pwa-ml-dbdeb.firebaseapp.com",
     projectId: "pwa-ml-dbdeb",
@@ -8,21 +12,21 @@ const firebaseConfig = {
     messagingSenderId: "872539464589",
     appId: "1:872539464589:web:803a3bf73e27e8d7ee93c4",
     measurementId: "G-9FXY1WGQEJ"
-  };
-console.log('loded main')
-const app = initializeApp(firebaseConfig);
+  });
 
-const messaging = getMessaging(app);
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging(firebaseApp);
 
 
 messaging.onBackgroundMessage((payload) => {
     console.log('[firebase-messaging-sw.js] Received background message ', payload);
     // Customize notification here
-    const notificationTitle = payload.title;
-    const notificationOptions = {
-      body:payload.body,
-      icon: '/firebase-logo.png'
-    };
+    // const notificationTitle = payload.title;
+    // const notificationOptions = {
+    //   body:payload.body,
+    //   icon: '/firebase-logo.png'
+    // };
     // set("alot_number",payload.body)
     self.registration.showNotification("Welcome to TCE");
   });
